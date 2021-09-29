9ba02ec5cee780be568226256b62b99941012448
jmhar <jmhar@protonmail.com>
2021-09-29 14:40:43 +1000

    get pages

--- lib/apocalypto/scraper.rb
+++ lib/apocalypto/scraper.rb
@@ -1,8 +1,25 @@
+require_relative '../apocalypto_app.rb'
+
 class ApocalyptoApp::Scraper
     COVID_URL = "https://www.worldometers.info/coronavirus/#countries"
     WALMART_URL = "https://www.walmart.com/"
     FOOD = "search?q=premade+meals&catId=976759_976794_5614446_1797065&sort=best_seller"
     WEAPONS = "search?q=guns&catId=4125_546956_1390220_2909700_1721755&min_price=0&sort=best_seller"
+
+    def get_food_page(url = WALMART_URL+FOOD)
+        uri = URI.parse(url)
+        puts Nokogiri::HTML(uri.open)
+    end
+
+    def get_weapon_page(url = WALMART_URL+WEAPONS)
+        uri = URI.parse(url)
+        puts Nokogiri::HTML(uri.open)
+    end
+
+    def get_country_page(url = COVID_URL)
+        uri = URI.parse(url)
+        puts Nokogiri::HTML(uri.open)
+    end
 
     def get_supplies
     end
