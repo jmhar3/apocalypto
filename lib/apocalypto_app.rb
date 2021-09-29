7c80846c3aedca3fe57466faaeed875441e04baf
jmhar <jmhar@protonmail.com>
2021-09-29 13:46:11 +1000

    add global methods

--- lib/apocalypto_app.rb
+++ lib/apocalypto_app.rb
@@ -1,4 +1,27 @@
 module ApocalyptoApp
+    def exit
+        puts "Enter any key to escape the apocalypse."
+    end
+    
+    def new_line
+        puts ""
+    end
+
+    def divider
+        puts "══════════ ∘◦ ❈ ◦∘ ══════════"
+    end
+
+    def zombie
+        puts "‿︵‿︵(ಥ﹏ಥ)‿︵‿︵"
+    end
+
+    def oh_no
+        puts "ᵒʰ(⑉・̆⌓・̆⑉)ɴᴏ"
+    end
+
+    def hit
+        puts "(ﾉ>｡☆)ﾉ"
+    end
 end
 
 require_relative '../config/environment.rb'
